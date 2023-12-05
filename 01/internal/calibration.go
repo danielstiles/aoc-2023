0b80615bf1eba492c9aedf0a6521498926e2711c
Daniel Stiles <22626981+danielstiles@users.noreply.github.com>
2023-12-05 09:29:49 -0500

    feat: calibration package

---
+++ 01/internal/calibration.go
@@ -0,0 +1,22 @@
+package calibration
+
+func GetCalibration(line []byte) int {
+	out := 0
+	var first, last bool
+	l := len(line)
+	for i, b := range line {
+		b2 := line[l-i-1]
+		if !first && b >= '0' && b <= '9' {
+			out += int(b) - 48
+			first = true
+		}
+		if !last && b2 >= '0' && b2 <= '9' {
+			out += int(b2) - 48
+			last = true
+		}
+		if first && last {
+			break
+		}
+	}
+	return out
+}
