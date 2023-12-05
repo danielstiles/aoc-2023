dc54eae6d0ba1ef82d09e179a8d27b8cd101edc8
Daniel Stiles <22626981+danielstiles@users.noreply.github.com>
2023-12-05 10:08:29 -0500

    fix: dir structure

--- 01/internal/calibration/calibration.go
+++ 01/internal/calibration/calibration.go
@@ -7,7 +7,7 @@ func GetCalibration(line []byte) int {
 	for i, b := range line {
 		b2 := line[l-i-1]
 		if !first && b >= '0' && b <= '9' {
-			out += int(b) - 48
+			out += (int(b) - 48) * 10
 			first = true
 		}
 		if !last && b2 >= '0' && b2 <= '9' {
