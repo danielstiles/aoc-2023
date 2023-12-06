fab76ef760c445bae0c08ce5888b070ebee44113
Daniel Stiles <22626981+danielstiles@users.noreply.github.com>
2023-12-05 21:44:30 -0500

    feat: day one part two

---
+++ 01/cmd/part_2/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"os"
+
+	"golang.org/x/exp/slog"
+
+	"github.com/danielstiles/aoc-2023/01/internal/calibration"
+)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		slog.Error("Could not read file", slog.Any("error", err))
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	total := 0
+	for fileScanner.Scan() {
+		total += calibration.GetV2Calibration(fileScanner.Text())
+	}
+	slog.Info("Answer", slog.Int("total", total))
+}
--- 01/internal/calibration/calibration.go
+++ 01/internal/calibration/calibration.go
@@ -1,5 +1,12 @@
 package calibration
 
+import (
+	"regexp"
+	"unicode/utf8"
+
+	"golang.org/x/exp/slog"
+)
+
 func GetCalibration(line string) int {
 	out := 0
 	var first, last bool
@@ -19,4 +26,73 @@ func GetCalibration(line string) int {
 		}
 	}
 	return out
+}
+
+var digits = map[string]int{
+	"0":     0,
+	"zero":  0,
+	"1":     1,
+	"one":   1,
+	"2":     2,
+	"two":   2,
+	"3":     3,
+	"three": 3,
+	"4":     4,
+	"four":  4,
+	"5":     5,
+	"five":  5,
+	"6":     6,
+	"six":   6,
+	"7":     7,
+	"seven": 7,
+	"8":     8,
+	"eight": 8,
+	"9":     9,
+	"nine":  9,
+}
+
+var forward, backward *regexp.Regexp
+
+func GetV2Calibration(line string) int {
+	if forward == nil {
+		buildRegex()
+	}
+	first := forward.FindString(line)
+	if first == "" {
+		return 0
+	}
+	last := backward.FindString(reverseString(line))
+	return digits[first]*10 + digits[reverseString(last)]
+}
+
+func buildRegex() {
+	r := ""
+	first := true
+	for name := range digits {
+		if !first {
+			r += "|"
+		}
+		first = false
+		r += name
+	}
+	var err error
+	forward, err = regexp.Compile(r)
+	if err != nil {
+		slog.Error("Could not compile forward regex", slog.Any("error", err))
+	}
+	backward, err = regexp.Compile(reverseString(r))
+	if err != nil {
+		slog.Error("Could not compile backward regex", slog.Any("error", err))
+	}
+}
+
+func reverseString(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
 }
